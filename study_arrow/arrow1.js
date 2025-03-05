ec6042e2c7f11c9fce104e4e6eaa73ac3631e033
ddiske <ddiske@naver.com>
2025-03-05 17:44:01 +0900

    Day46_arrowFunction

---
+++ study_arrow/arrow1.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("btn");
+
+// arrow function
+// 화살표 함수 ->
+
+function (){
+    // 
+}
+()=>{}
+
+function(p1){}
+(p1)=>{}
+p1 => {}
+
+function(p1, p2) {}
+(p1, p2) => {}
