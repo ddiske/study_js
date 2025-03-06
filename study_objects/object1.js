6cff736c7a7453f983fa86b7661cc7e1db78bbde
ddiske <ddiske@naver.com>
2025-03-06 11:25:07 +0900

    Day47_Object

---
+++ study_objects/object1.js
@@ -0,0 +1,10 @@
+console.log("host", location.host);
+console.log("hostname", location.hostname);
+console.log("href", location.href);
+console.log("origin", location.origin);
+console.log("pathname", location.pathname);
+console.log("port", location.port);
+console.log("protocol", location.protocol);
+
+location.search = null;
+console.log("search",location.search);
