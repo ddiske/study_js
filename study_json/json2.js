b745fdb897f012de474daeb87527220e4854fe47
ddiske <ddiske@naver.com>
2025-03-13 19:17:47 +0900

    Day52_json+modal

--- study_json/json2.js
+++ study_json/json2.js
@@ -3,7 +3,7 @@ const getList = document.getElementById("getList")
 const table = document.createElement("table")
 table.appendChild(document.createElement("thead")).setAttribute("id", "thead")
 table.appendChild(document.createElement("tbody")).setAttribute("id", "tb")
-table.setAttribute("class", "table table-striped")
+table.setAttribute("class", "table table-striped table-success")
 
 getList.addEventListener("click", ()=>{
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -18,13 +18,50 @@ getList.addEventListener("click", ()=>{
         th.appendChild(document.createElement("th")).append("title")
         th.appendChild(document.createElement("th")).append("body")
         let tb = document.getElementById("tb")
+        
         r.forEach((element, idx) => {
             tb.appendChild(document.createElement("tr")).setAttribute("id", "tr"+idx)
             let td = document.getElementById("tr"+idx)
-            td.appendChild(document.createElement("td")).append(element.userId)
-            td.appendChild(document.createElement("td")).append(element.id)
-            td.appendChild(document.createElement("td")).append(element.title)
-            td.appendChild(document.createElement("td")).append(element.body)
-        });
+            let t1 = td.appendChild(document.createElement("td"))
+            t1.append(element.userId)
+            let t2 = td.appendChild(document.createElement("td"))
+            t2.append(element.id)
+            t2.setAttribute("id", "ti"+idx)
+            let a = document.createElement("a")
+            a.innerText = element.title
+            a.setAttribute("data-bs-toggle", "modal")
+            a.setAttribute("data-bs-target", "#exampleModal")
+            a.setAttribute("id", element.id)
+            a.classList.add("ti")
+            
+            let t3 = td.appendChild(document.createElement("td"))
+            t3.append(a)
+            let t4 = td.appendChild(document.createElement("td"))
+            t4.append(element.body)
+        })
     })
+    
 })
+
+result.addEventListener("click", (e)=>{
+    let t_title = document.getElementById("t_title")
+    let t_body = document.getElementById("t_body")
+    let t_id = document.getElementById("t_id")
+
+    if(e.target.classList.contains('ti')){
+        modalClick(e.target.getAttribute("id"))
+    }
+
+})
+
+function modalClick(num){
+    fetch('https://jsonplaceholder.typicode.com/posts/'+num)
+    .then(r=>r.json())
+    .then(json=>{
+        t_title.innerText=json.title
+        t_body.innerText=json.body
+        t_id.innerText=json.userId+"_"+json.id
+    })
+}
+
+// data-bs-toggle="modal" data-bs-target="#exampleModal"
